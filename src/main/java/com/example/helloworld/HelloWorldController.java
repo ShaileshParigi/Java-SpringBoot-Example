65df73889e7d22690623ec167726f7aeb3a6b323
ShaileshParigi <shaileshparigi13@gmail.com>
2023-03-01 16:02:42 +0000

    Example hello world

---
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.example.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController{
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World";
+    }
+    @GetMapping("/name")
+    public String name(){
+        return "Shailesh";
+    }
+}
